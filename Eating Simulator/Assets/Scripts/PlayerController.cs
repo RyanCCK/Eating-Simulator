e779fb381970e18a3db8705d453e2f3d028314ed
RyanCCK <RyanCK.Dev@gmail.com>
2022-05-30 04:41:17 -0700

    Added terrain that decays and breaks over time

--- Eating Simulator/Assets/Scripts/PlayerController.cs
+++ Eating Simulator/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 // Script to control the player character
 
+[RequireComponent(typeof(Rigidbody))]
+[DisallowMultipleComponent]
 public class PlayerController : MonoBehaviour
 {
-    // Global variable BAD?
-    [SerializeField] float playerSpeed = 5f;
+    // Global variables BAD?
+    [SerializeField] public float playerSpeed = 5f;
+    [SerializeField] public float jumpForce = 3.5f;
+    public Vector3 jump;
+    public bool isGrounded;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, 2f, 0f);
     }
 
     // Update is called once per frame
@@ -21,12 +28,27 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
     }
 
-    // Handles movement based on player input
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+
+    // Handles lateral movement using transform.translate based on player input 
     void PlayerMovement()
     {
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
-
         transform.Translate(x, 0, z);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
---
+++ Eating Simulator/Assets/Scripts/TerrainDecay.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class TerrainDecay : MonoBehaviour
+{
+    private bool isDecaying;
+    /////////////TODO/////////////TODO/////////////TOOD/////////////
+    //TODO: Fix these states and the associated switch statement in the Decay loop
+    /////////////TODO/////////////TODO/////////////TOOD/////////////
+    private enum DecayStates
+    {
+        initial,
+        warning,
+        final,
+        destroy,
+        destroyed
+    };
+    private DecayStates state;
+    [SerializeField] public float decayDelay = 1.5f;
+    [SerializeField] public Material initialMaterial;
+    [SerializeField] public Material warningMaterial;
+    [SerializeField] public Material finalMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDecaying = false;
+        state = DecayStates.initial;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!isDecaying && other.gameObject.tag == "Player")
+        {
+            isDecaying = true;
+            StartCoroutine(Decay());
+        }
+    }
+    /////////////TODO/////////////TODO/////////////TOOD/////////////
+    //TODO: fix this loop to use fewer states and be cleaner
+    /////////////TODO/////////////TODO/////////////TOOD/////////////
+    IEnumerator Decay()
+    {
+        // Update the state of the tile, and modify its appearance
+        while (state != DecayStates.destroyed)
+        {
+            switch (state)
+            {
+            case DecayStates.initial:
+                {
+                    GetComponent<MeshRenderer>().material = warningMaterial;
+                    state = DecayStates.warning;
+                    break;
+                }
+            case DecayStates.warning:
+                {
+                    GetComponent<MeshRenderer>().material = finalMaterial;
+                    state = DecayStates.final;
+                    break;
+                }
+            case DecayStates.final:
+                {
+                    gameObject.AddComponent(typeof(Rigidbody));
+                    Destroy(GetComponent<Collider>());
+                    state = DecayStates.destroy;
+                    break;
+                }
+            case DecayStates.destroy:
+                {
+                    Destroy(gameObject);
+                    state = DecayStates.destroyed;
+                    break;
+                }
+            }
+
+            yield return new WaitForSeconds(decayDelay);
+        }
+    }
+}

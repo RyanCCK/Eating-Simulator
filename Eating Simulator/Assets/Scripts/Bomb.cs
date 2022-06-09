308a5c5d099d2a3c2584adb73509b004312fdb9b
RyanCCK <RyanCK.Dev@gmail.com>
2022-06-08 20:11:08 -0700

    Added Bomb Tile
    Added bomb tile, which destroys nearby destructible tiles when collided with

--- Eating Simulator/Assets/Scripts/Bomb.cs
+++ Eating Simulator/Assets/Scripts/Bomb.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    
+    [SerializeField] public float explosionRadius = 3f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, explosionRadius);
+            foreach (var collider in colliders)
+            {
+                if (collider.gameObject.tag == "Destructible")
+                    Destroy(collider.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
 }

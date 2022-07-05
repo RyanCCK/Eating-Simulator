826e137c7e768a14d98fae1ec4adb408c84acd6b
RyanCCK <RyanCK.Dev@gmail.com>
2022-07-05 09:57:17 -0700

    Fixed Terrain Rotation
    Fixed terrain rotation script to rotate about actual center point of parent object

--- Eating Simulator/Assets/Scripts/TerrainRotator.cs
+++ Eating Simulator/Assets/Scripts/TerrainRotator.cs
@@ -7,15 +7,28 @@ public class TerrainRotator : MonoBehaviour
     [SerializeField] public float xSpeed = 0f;
     [SerializeField] public float ySpeed = 0f;
     [SerializeField] public float zSpeed = 0f;
-    private Vector3 rotationVector;
-    
+    private Vector3 center;
+
+
+    private void Awake()
+    {
+        //compute actual midpoint of parent object from all child object positions
+        foreach(Transform child in transform)
+        {
+            center += child.position;
+        }
+        center /= transform.childCount;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        rotationVector.x = xSpeed;
-        rotationVector.y = ySpeed;
-        rotationVector.z = zSpeed;
-        transform.Rotate(rotationVector * Time.deltaTime, Space.Self);
+        //x rotation
+        gameObject.transform.RotateAround(center, Vector3.right, xSpeed * Time.deltaTime);
+        //y rotation
+        gameObject.transform.RotateAround(center, Vector3.up, ySpeed * Time.deltaTime);
+        //z rotation
+        gameObject.transform.RotateAround(center, Vector3.forward, zSpeed * Time.deltaTime);
     }
 }
